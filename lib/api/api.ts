9ad57cc6a91a2b536aeb174c557c1984401d07b0
ibuizle <ibuizle@gmail.com>
2026-02-22 19:00:38 +0200

    Update api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,13 +1,6 @@
 import axios from 'axios';
 
-// На проді/прев’ю будемо ходити на той самий origin, де відкритий сайт.
-// Це гарантує, що cookies працюють.
-const origin =
-  typeof window !== 'undefined'
-    ? window.location.origin
-    : process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000';
-
-const baseURL = `${origin}/api`;
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 
 export const api = axios.create({
   baseURL,
